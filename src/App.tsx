61019cacc32d63cbc60211b9a8cb26fd223543e5
Shoaib-Programmer <shoaibnigam422@gmail.com>
2025-09-23 07:25:23 +0000

    Fixed scroll issues

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "react-hot-toast";
 
 export default function App() {
     return (
-        <div className="min-h-[100dvh] min-w-[100vw] w-full bg-[url('/images/background.jpg')] bg-cover bg-center bg-no-repeat flex flex-col items-stretch justify-start text-black overflow-hidden">
+        <div className="h-full w-full bg-[url('/images/background.jpg')] bg-cover bg-center bg-no-repeat flex flex-col items-stretch justify-start text-black">
             <TrashSorterGame />
             <Toaster />
         </div>
