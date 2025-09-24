777015a319f948235c3798308bbe3751055aaf2a
Shoaib-Programmer <shoaibnigam422@gmail.com>
2025-09-24 13:45:28 +0000

    Fixed path prefix in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: process.env.NODE_ENV === "production" ? "/trash-sorter-park/" : "/",
+    base: process.env.NODE_ENV === "production" ? "/Trash-Sorter-Park/" : "/",
 });
